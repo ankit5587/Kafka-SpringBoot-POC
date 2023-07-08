ef760e9941912e0cd8a3e85c69ec3ee9929803ca
ankit5587 <ankit5587@users.noreply.github.com>
2023-07-08 13:29:42 +0530

    Merge remote changes

---
+++ src/test/java/org/ankit/kafkaspringdemo/KafkaSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ankit.kafkaspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
